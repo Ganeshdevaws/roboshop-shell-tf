cd81ebd391af9555cdc2c9670437ab87217e06a3
ganesh <ganeshjanapareddy2019@gmail.com>
2023-10-28 14:45:31 +0530

    update

--- web.sh
+++ web.sh
@@ -54,7 +54,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 
 VALIDATE $? "Unzipping frontend artifacts"
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
+cp /home/centos/roboshop-shell-tf/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
 
 VALIDATE $? "Copying roboshop config"
 
